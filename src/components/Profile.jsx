3c2b4ffc1da05eccc540353ade442c57f36ae6d0
Steve Abbott <ttsabbott@gmail.com>
2025-01-24 09:42:25 -0700

    Update Profile.jsx

--- src/components/Profile.jsx
+++ src/components/Profile.jsx
@@ -5,7 +5,7 @@ import { useAuth0 } from '@auth0/auth0-react';
 import Container from '@mui/material/Container';
 
 const Profile = () => {
-    const { user } = useAuth0();
+    const { user, user_metadata, app_metadata } = useAuth0();
     return (
         <Container>
             <img src={user.picture} alt="Profile" style={{ float: "right" }} />
@@ -15,7 +15,12 @@ const Profile = () => {
             <p id="userFamily">Family Name: {user.family_name ? user.family_name : 'n/a'}</p>
             <p id="userEmail">Email: {user.email ? user.email : 'n/a'}</p>
             <p id="userSub">Sub: {user.sub ? user.sub : 'n/a'}</p>
-            <pre>{JSON.stringify(user, null, 2)}</pre>
+            <pre>user
+                {JSON.stringify(user, null, 2)}</pre>
+            <pre>user_metadata
+                {JSON.stringify(user_metadata, null, 2)}</pre>
+            <pre>app_metadata
+                {JSON.stringify(app_metadata, null, 2)}</pre>
         </Container>
     );
 };
